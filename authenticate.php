76ad7fe95d98622b7763b4d549a6c44acae869a5
Joel Maus <jmaus06@yahoo.com>
2022-02-02 22:02:46 -0500

    php authenticate files

---
+++ authenticate.php
@@ -0,0 +1,9 @@
+<html>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<form action="index.php" method="POST">
+<input type="password" name="pass">
+<input type="submit" value="Sign in">
+</form>
+
+
+</html>
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+if ($_POST['pass']=='capstone'){
+}
+else
+header('Location:authenticate.php');
+?>
+<html>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<body>
+    You are now in. You have entered the correct password.
+    </body>
+    </html>
